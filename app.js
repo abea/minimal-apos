abcc87640b477b8da72deec106c4939a52c0e598
Alex Bea <alex.bea@gmail.com>
2019-06-20 15:14:22 -0500

    Cleans up to boilerplate

--- app.js
+++ app.js
@@ -8,35 +8,6 @@ var apos = require('apostrophe')({
   // configuring user accounts.
 
   modules: {
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
-    'apostrophe-override-options': {},
-    'apostrophe-forms': {
-      // emailSubmissions: false
-    },
-    'apostrophe-forms-widgets': {},
-    'apostrophe-forms-text-field-widgets': {},
-    'apostrophe-forms-textarea-field-widgets': {},
-    'apostrophe-forms-select-field-widgets': {},
-    'apostrophe-forms-radio-field-widgets': {},
-    'apostrophe-forms-checkboxes-field-widgets': {},
-    'apostrophe-forms-file-field-widgets': {},
-    // 'apostrophe-forms-params-field-widgets': {},
-    // 'apostrophe-forms-boolean-field-widgets': {},
-    'apostrophe-permissions': {
-      construct: function (self, options) {
-        // Needed if you want file fields to work on public pages
-        self.addPublic(['edit-attachment']);
-      }
-    },
-    'apostrophe-email': {
-      // See the nodemailer documentation, many
-      // different transports are available, this one
-      // matches how PHP does it on Linux servers
-      nodemailer: {
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail'
-      }
-    }
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
   }
 });
